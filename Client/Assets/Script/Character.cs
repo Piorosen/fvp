faf13066b6bc5704fec940db39758a7847d4814c
Piorosen <aoikazto@gmail.com>
2018-11-03 01:25:36 +0900

    캐릭터 이동시 가속 기능 추가

--- Client/Assets/Script/Character.cs
+++ Client/Assets/Script/Character.cs
@@ -1,84 +1,111 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+
 using UnityEngine;
 
 public class Character : MonoBehaviour {
 
-    public float Speed;
+    public float MaxSpeed;
     public float Jump;
+    public float Accelerate = 3.0f;
+    public float Speed = 0;
 
     Animator anime;
     Rigidbody2D rigidBody;
-
     SpriteRenderer Renderer;
-
     BoxCollider2D playerCollider;
 
-	// Use this for initialization
-	void Start () {
+    int? UID = null;
+
+    void Awake()
+    {
+        UID = this.GetHashCode();
+    }
+
+    void Start () {
         anime = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
         Renderer = GetComponent<SpriteRenderer>();
         playerCollider = GetComponent<BoxCollider2D>();
 	}
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (rigidBody.velocity.y > 0)
+        {
+            anime.SetBool("Jump", true);
+        }
+        else if (rigidBody.velocity.y < 0)
+        {
+            anime.SetBool("Down", true);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Attach" + collision.gameObject.layer);
         anime.SetBool("Jump", false);
         anime.SetBool("Down", false);
-        playerCollider.enabled = true;
     }
 
     // Update is called once per frame
     void FixedUpdate () {
         transform.rotation = Quaternion.identity;
-
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         Vector3 Movement = new Vector3(x, y, this.transform.position.z);
 
-        anime.SetFloat("DivX", x);
-        anime.SetFloat("DivY", y);
+        float Acc = Accelerate * Time.deltaTime;
 
-        if (x != 0)
+        if (x < 0)
         {
-            if (x > 0)
+            if (Speed < -MaxSpeed)
             {
-                Renderer.flipX = true;
+                Speed = -MaxSpeed;
             }
-            else if (x < 0)
+            else
             {
-                Renderer.flipX = false;
+                Speed -= Acc;
             }
-            anime.SetBool("Walking", true);
         }
-        else
+        else if (x > 0)
         {
-            anime.SetBool("Walking", false);
+            if (Speed > MaxSpeed)
+            {
+                Speed = MaxSpeed;
+            }
+            else
+            {
+                Speed += Acc;
+            }
+        }else{
+            if (Speed > 0.01)
+            {
+                Speed -= Acc * 2;
+            }
+            else if (Speed < -0.01)
+            {
+                Speed += Acc * 2;
+            }
+            else{
+                Speed = 0.0f;
+            }
         }
 
+        if (Speed > 0){
+            Renderer.flipX = true;
+        }else if (Speed < 0){
+            Renderer.flipX = false;
+        }
 
-        if (y != 0)
+        if (!anime.GetBool("Jump"))
         {
             if (y > 0)
             {
-                rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0);
                 rigidBody.AddForce(new Vector2(0, Jump));
-                anime.SetBool("Jump", true);
-            }else  if (y < 0){
-                playerCollider.enabled = false;
-                anime.SetBool("Down", true);
-
             }
-
-            anime.SetBool("IsFly", true);
-        }
-        else if (rigidBody.velocity.y >= 0)
-        {
-            anime.SetBool("IsFly", false);
         }
 
-        transform.Translate(Movement.x * Speed * Time.deltaTime, 0, 0);
+        rigidBody.transform.Translate(Vector3.right * Speed);
 	}
 }
