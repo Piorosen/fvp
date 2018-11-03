ed41752ae52efd4f900a9a8fe9ca4930a72036d1
AK(Piorosen) <aoikazto@naver.com>
2018-11-03 10:37:14 +0900

    NGUI 버그 수정
    버그 수정 및 파일 수정

---
+++ Client/Assets/Script/PlayerPool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPool : UnityEngine.MonoBehaviour
+{
+    public GameObject[] Prefab = new GameObject[1];
+
+    private GameObject[] Pool = new GameObject[8];
+
+    void Awake(){
+
+    }
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+
+    void AddPlayer(GameObject @object){
+        
+    }
+
+    void DelPlayer(GameObject @object){
+
+    }
+    void DelPlayer(int UID){
+
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
