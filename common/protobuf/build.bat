4602c304019082e534d3206df43083f706f60e4b
zepeh92 <zepeh92@gmail.com>
2018-11-25 20:15:36 +0900

    경로 변경

--- common/protobuf/build.bat
+++ common/protobuf/build.bat
@@ -1,2 +1,2 @@
-protoc.exe -I=./ --cpp_out=../../server/packet --csharp_out=../../Client/Assets/Script packet_data.proto packet_type.proto packet_internal.proto packet.proto
+protoc.exe -I=./ --cpp_out=../../server/packet --csharp_out=../../Client/Assets/Scripts packet_data.proto packet_type.proto packet_internal.proto packet.proto
 pause
