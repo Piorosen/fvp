0c49ae4b2185c29325b1c2413a113b956f6b231b
Piorosen <aoikazto@gmail.com>
2018-12-31 15:30:02 +0900

    맵수정

---
+++ Client/Assets/Scripts/InGame/Game/Status.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    public UnityEngine.UI.Image Statuses;
+    
+    public void EventHandler()
+    {
+        Statuses.enabled = !Statuses.enabled;
+    }
+    
+
+
+}
