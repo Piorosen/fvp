ee3a3c0fcc03652db49f2be0bd708b9ecdd69193
DaeJin Jeong <devwithu@naver.com>
2019-01-11 20:12:40 +0900

    유니티 에디터에서 Play 종료시 네트워크 끊도록 수정

---
+++ Client/Assets/Editor/EditorQuit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoadAttribute]
+public class EditorQuit : MonoBehaviour
+{
+    static bool WantsToQuit()
+    {
+        Debug.Log("Player  quitting. NetworkManager.Instance.Close()");
+        NetworkManager.Instance.Close();
+        return true;
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    static void RunOnStart()
+    {
+        Application.wantsToQuit += WantsToQuit;
+    }
+}
